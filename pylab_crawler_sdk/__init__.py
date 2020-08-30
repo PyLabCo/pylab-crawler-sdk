ab18de54219b433c1fa8e9c8b5d11950f6c11415
Yeongbin Jo <yeongbin.jo@pylab.co>
2020-08-31 08:32:01 +0900

    1.0.1

--- pylab_crawler_sdk/__init__.py
+++ pylab_crawler_sdk/__init__.py
@@ -9,6 +9,8 @@ class Session(object):
 
     def add_log(self, task_id, content):
         """로그를 기록합니다"""
+        if not task_id:
+            return
         try:
             res = requests.post(f'{BASE_URL}/sdk/task-logs/?key={self.key}', data={
                 'task': task_id,
@@ -19,6 +21,8 @@ class Session(object):
             pass
 
     def is_running(self, task_id):
+        if not task_id:
+            return True
         try:
             res = requests.get(f'{BASE_URL}/sdk/task-terminations/?key={self.key}&task={task_id}')
             res.raise_for_status()
--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ elif sys.argv[-1] == 'clean':
 
 setup(
     name="pylab-crawler-sdk",
-    version="1.0.0",
+    version="1.0.1",
     author="PyLab",
     author_email="yeongbin.jo@pylab.co",
     description="SDK for https://crawler.pylab.co",
